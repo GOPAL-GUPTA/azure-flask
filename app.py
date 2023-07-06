1d50434c5c563469cffd919fe04c5338b74d1df1
GOPAL-GUPTA <45972364+GOPAL-GUPTA@users.noreply.github.com>
2023-07-06 07:23:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,48 @@
-from flask import Flask
+# from flask import Flask
+
+# app =Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'hello world for azure'
+# # @app.route('/first')
+# # def first():
+# #     # if(request.method == 'GET'):
+# #     #     data = {
+# #     #         "Modules" : 15,
+# #     #         "Subject" : "Data Structures and Algorithms",
+# #     #     }
+# #     #     return jsonify(data)
+    
+# @app.route('/webhook')
+# def webhook():
+#    return 'webhook'
+        
+# if __name__=='__main__':
+#     app.run()
+
+from flask import Flask,request,abort,jsonify
 
 app =Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world for azure'
-# @app.route('/first')
-# def first():
-#     # if(request.method == 'GET'):
-#     #     data = {
-#     #         "Modules" : 15,
-#     #         "Subject" : "Data Structures and Algorithms",
-#     #     }
-#     #     return jsonify(data)
+    return 'hello world'
+@app.route('/first')
+def first():
+    if(request.method == 'GET'):
+        data = {
+            "Modules" : 15,
+            "Subject" : "Data Structures and Algorithms",
+        }
+        return jsonify(data)
     
-@app.route('/webhook')
+@app.route('/webhook',methods =['GET','POST'])
 def webhook():
-   return 'webhook'
+    if request.method=='POST':
+        print(request.json)
+        data = request.json
+        return 'success'
         
 if __name__=='__main__':
     app.run()
